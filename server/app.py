f6f12e16739932a3d7ecb01d7b0d41acfa950772
brewchetta <chett.tiller@gmail.com>
2024-07-11 14:48:58 -0400

    update config, remove solution code

--- server/app.py
+++ server/app.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python3
 
-import os
-from flask_bcrypt import Bcrypt
-from flask import Flask, jsonify, request, session
-from flask_migrate import Migrate
-
+from flask import request, session
 from models import db, User, Note
+from config import app
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-bcrypt = Bcrypt(app)
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
 
 URL_PREFIX = '/api'
 
@@ -32,10 +14,8 @@ URL_PREFIX = '/api'
 def create_user():
     try:
         new_user = User(username=request.json['username'])
-        new_user._hashed_password = bcrypt.generate_password_hash(request.json['password']).decode('utf-8')
         db.session.add(new_user)
         db.session.commit()
-        session['user_id'] = new_user.id
         return new_user.to_dict(), 201
     except Exception as e:
         return { 'error': str(e) }, 406
@@ -43,39 +23,17 @@ def create_user():
 
 # CHECK SESSION #
 
-@app.get(URL_PREFIX + '/check-session')
-def check_session():
-    user = User.query.where(User.id == session['user_id']).first()
-    if user: 
-        return user.to_dict(), 200
-    else:
-        return {}, 204
-
+# ???
 
 # SESSION LOGIN/LOGOUT#
 
-@app.post(URL_PREFIX + '/login')
-def login():
-    user = User.query.where(User.username == request.json.get('username')).first()
-    if user and bcrypt.check_password_hash(user._hashed_password, request.json.get('password')):
-        session['user_id'] = user.id
-        return user.to_dict(), 201
-    else:
-        return {'error': 'Username or password was invalid'}, 401
-
-
-@app.delete(URL_PREFIX + '/logout')
-def logout():
-    session.pop('user_id')
-    return {}, 204
-
+# ???
 
 # EXAMPLE OTHER RESOURCES #
 
 @app.get(URL_PREFIX + '/notes')
 def get_notes():
-    user = User.query.where(User.id == session['user_id']).first()
-    return [note.to_dict() for note in user.notes], 200
+    return [note.to_dict() for note in Note.query.all()], 200
 
 @app.post(URL_PREFIX + '/notes')
 def create_note():
@@ -84,9 +42,9 @@ def create_note():
         new_note = Note(**data)
         db.session.add(new_note)
         db.session.commit()
-        return jsonify( new_note.to_dict() ), 201
+        return new_note.to_dict(), 201
     except Exception as e:
-        return jsonify( {'error': str(e)} ), 406
+        return {'error': str(e)}, 406
 
 # APP RUN #
 
